02af1c85d72d8cd1b414fc945f3b48db60dd5f09
Maksim_Kiselev <laurkurt@gmail.com>
2022-11-04 19:36:12 +0300

    getTheValueOfTheThird-to-lastLetterAndChangePre

--- JS/FreeCodeCampJS.js
+++ JS/FreeCodeCampJS.js
@@ -73,4 +73,10 @@ const thirdLetterOfLastName = lastName[2]; // Change this line
 // Setup
 const lastName1 = "Lovelace";
 // Only change code below this line
-const lastLetterOfLastName = lastName[lastName1.length-1]; // Change this line
+const lastLetterOfLastName = lastName1[lastName1.length-1]; // Change this line
+// get the value of the second-to-last letter
+
+const lastName2 = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName2[lastName2.length-2]; // Change this line
