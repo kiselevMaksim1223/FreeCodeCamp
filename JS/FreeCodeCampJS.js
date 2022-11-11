1e75d9d990875ae297350011a672a582f044e41b
Maksim_Kiselev <laurkurt@gmail.com>
2022-11-11 15:14:41 +0300

    operator > (convert on one type)

--- JS/FreeCodeCampJS.js
+++ JS/FreeCodeCampJS.js
@@ -173,3 +173,14 @@ function testStrictNotEqual(val) {
     }
     return "Equal";
 }
+
+// operator > (convert on one type)
+function testGreaterThan(val) {
+    if (val > 100) { // Change this line
+        return "Over 100";
+    }
+    if (val > 10) { // Change this line
+        return "Over 10";
+    }
+    return "10 or Under";
+}
