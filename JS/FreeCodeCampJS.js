379f8021e1d02a516bc11d7d458f46d373aef18f
Maksim_Kiselev <laurkurt@gmail.com>
2022-11-11 15:18:20 +0300

    operator <=

--- JS/FreeCodeCampJS.js
+++ JS/FreeCodeCampJS.js
@@ -196,11 +196,22 @@ function testGreaterOrEqual(val) {
 }
 // <
 function testLessThan(val) {
-    if (val < 25) { 
+    if (val < 25) {
         return "Under 25";
     }
-    if (val < 55) { 
+    if (val < 55) {
         return "Under 55";
     }
     return "55 or Over";
 }
+// <=
+function testLessOrEqual(val) {
+    if (val <= 12) { 
+        return "Smaller Than or Equal to 12";
+    }
+    if (val <= 24) { 
+        return "Smaller Than or Equal to 24";
+    }
+    return "More Than 24";
+}
+// 
