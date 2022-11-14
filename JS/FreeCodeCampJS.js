81ec9bcedcb9747059a254de606a99bb364f6f43
Maksim_Kiselev <laurkurt@gmail.com>
2022-11-14 15:38:16 +0300

    switch case

--- JS/FreeCodeCampJS.js
+++ JS/FreeCodeCampJS.js
@@ -264,3 +264,23 @@ function golfScore(par, strokes) {
         return "Go Home!"
     }
 }
+
+// switch case
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+}
